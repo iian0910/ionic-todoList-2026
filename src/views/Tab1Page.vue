eaec4ec27b47450a765ad48f3bdbe282fc033a23
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2026-01-19 10:43:38 +0800

    基礎架構設置 / Tab bar 項目設置

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,23 +1,53 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+    <ion-menu content-id="main-content">
+      <ion-header class="ion-no-border">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title>Menu Content</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
+      <ion-content class="ion-padding">This is the menu content.</ion-content>
+    </ion-menu>
+  
+    <ion-page id="main-content">
+      <ion-header class="ion-no-border page_title">
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-menu-button></ion-menu-button>
+          </ion-buttons>
+          <ion-title>Tab 1</ion-title>
+          <ion-buttons slot="end">
+            <ion-icon  class="search" aria-hidden="true" size="large" :icon="searchOutline"/>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content :fullscreen="true">
+        ...
+      </ion-content>
+    </ion-page>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonMenu,
+  IonMenuButton,
+  IonButtons,
+  IonIcon
+} from '@ionic/vue';
+import { searchOutline } from 'ionicons/icons';
 </script>
+
+<style lang="scss" scoped>
+.search {
+  padding: 8px;
+}
+.page_title {
+  text-align: center;
+}
+</style>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -2,27 +2,41 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar color="primary" slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon aria-hidden="true" :icon="homeOutline" />
+          <ion-label>首頁</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon aria-hidden="true" :icon="walletOutline" />
+          <ion-label>帳戶</ion-label>
         </ion-tab-button>
 
+        <ion-tab-button disabled/>
+
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon aria-hidden="true" :icon="barcodeOutline" />
+          <ion-label>發票</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon aria-hidden="true" :icon="statsChartOutline" />
+          <ion-label>圖表</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
+
+    <ion-fab slot="fixed" horizontal="center" vertical="bottom">
+      <ion-fab-button color="warning">
+        <ion-icon :icon="add"></ion-icon>
+      </ion-fab-button>
+    </ion-fab>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet, IonFab, IonFabButton } from '@ionic/vue';
+import { walletOutline, statsChartOutline, add, homeOutline, barcodeOutline } from 'ionicons/icons';
+
 </script>
