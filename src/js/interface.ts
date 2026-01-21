ebd86c67daa195749074939862339b0141bff422
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2026-01-21 17:43:42 +0800

    製作新增資料流程

---
+++ src/js/interface.ts
@@ -0,0 +1,7 @@
+export interface TodoItem {
+  id?: string,
+  date: string,
+  time: string,
+  content: string,
+  check: boolean
+}
--- src/views/AddTodoPage.vue
+++ src/views/AddTodoPage.vue
@@ -10,11 +10,76 @@
         </ion-toolbar>
       </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+      <div class="px-3">
+        <div class="my-3">
+          <div class="input_title w-20 flex self-center">日期</div>
+          <ion-input
+            :value="todo.date"
+            @update:modelValue="todo.date = $event"
+            label-placement="floating"
+            fill="outline"
+            id="open-date-modal"
+          />
+          <ion-modal
+            trigger="open-date-modal"
+            :initial-breakpoint="0.4"
+            :breakpoints="[0.4]"
+            handle-behavior="cycle"
+            @willPresent="onModalDateOpen"
+          >
+            <ion-content class="ion-padding">
+              <div class="ion-margin-top">
+                <ion-datetime
+                  presentation="date"
+                  :value="dateISO"
+                  @ionChange="onDateChange"
+                />
+              </div>
+            </ion-content>
+          </ion-modal>
+        </div>
+        <div class="my-3">
+          <div class="input_title w-20 flex self-center">時間</div>
+          <ion-input
+            :value="todo.time"
+            @update:modelValue="todo.time = $event"
+            label-placement="floating"
+            fill="outline"
+            id="open-time-modal"/>
+          
+          <ion-modal
+            trigger="open-time-modal"
+            :initial-breakpoint="0.3"
+            :breakpoints="[0.3]"
+            handle-behavior="cycle"
+            @willPresent="onModalTimeOpen"
+          >
+            <ion-content class="ion-padding">
+              <div class="ion-margin-top">
+                <ion-datetime
+                  presentation="time"
+                  :value="timeISO"
+                  @ionChange="onTimeChange"
+                />
+              </div>
+            </ion-content>
+          </ion-modal>
+        </div>
+        <div class="my-3">
+          <div class="input_title w-20 flex self-center">內容</div>
+          <ion-input
+            :value="todo.content"
+            @update:modelValue="todo.content = $event"
+            label-placement="floating"
+            fill="outline"/>
+        </div>
+
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-buttons slot="primary">
+            <ion-button fill="solid" @click="addDBInfo">新增</ion-button>
+          </ion-buttons>
         </ion-toolbar>
-      </ion-header>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -27,10 +92,74 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonButton,
   IonButtons,
-  IonIcon
+  IonIcon,
+  IonInput,
+  IonDatetime,
+  IonModal
 } from '@ionic/vue';
 import { arrowBackOutline } from 'ionicons/icons';
+import { ref } from 'vue';
+import dayjs from "dayjs"
+import db from '../js/firebaseDB';
+import {
+  addDoc,
+  collection
+} from "firebase/firestore";
+import { TodoItem } from '@/js/interface'
+
+// data
+const todo = ref<TodoItem>({
+  date: '',
+  time: '',
+  content: '',
+  check: false
+})
+const dateISO = ref<string>('')
+const timeISO = ref<string>('')
+
+// mounted
+const addDBInfo = async() => {
+  try {
+    await addDoc(collection(db, "todoList", todo.value.date, "todos"), {
+      date: todo.value.date,
+      time: todo.value.time,
+      content: todo.value.content,
+      check: false
+    });
+
+    console.log("新增成功");
+  } catch (error) {
+    console.error("新增失敗:", error);
+  }
+}
+
+const onModalDateOpen = () => {
+  dateISO.value = dayjs(new Date()).format("YYYY/MM/DDTHH:mm:ss")
+  todo.value.date = dayjs().format('YYYY-MM-DD')
+}
+
+const onModalTimeOpen = () => {
+  timeISO.value = dayjs(new Date()).format("YYYY/MM/DDTHH:mm:ss")
+  todo.value.time = dayjs().format('HH:mm')
+}
+
+const onDateChange = (e: CustomEvent) => {
+  const iso = e.detail.value
+  if (!iso) return
+
+  dateISO.value = iso                  // 給 ion-datetime
+  todo.value.date = dayjs(iso).format('YYYY-MM-DD') // 給自己
+}
+
+const onTimeChange = (e: CustomEvent) => {
+  const iso = e.detail.value
+  if (!iso) return
+
+  timeISO.value = iso                  // 給 ion-datetime
+  todo.value.time = dayjs(iso).format('HH:mm') // 給自己
+}
 
 const back = () => {
   router.push({path: '/tabs/home'})
@@ -47,5 +176,8 @@ const back = () => {
 }
 .page_title {
   text-align: center;
+}
+.input_title {
+  font-size: 18px;
 }
 </style>
