f2439e0a2c8e951edb0cfb1610d389c367479ebf
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2026-01-22 09:30:34 +0800

    修改專案名稱

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'bookkeeping',
+  appName: 'todo-list',
   webDir: 'dist'
 };
 
