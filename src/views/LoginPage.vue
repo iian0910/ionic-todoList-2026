5e75a26fceb22b28095049d4999d44b04f5f755e
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2026-01-28 17:25:38 +0800

    登入畫面改版(純畫面)

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,14 +1,29 @@
 <template>
   <ion-page>
     <ion-content class="ion-padding">
-      <div class="center-container ion-display-flex ion-justify-content-center ion-align-items-center">
-        <ion-button
-          @click="openGoogleLogin"
-          color="danger"
-          size="large"
-        >
-          GOOGLE 登入
-        </ion-button>
+      <div class="center-container">
+        <ion-title size="large" class="ion-text-center login-title">
+          Sign In
+        </ion-title>
+        <div class="ion-padding-vertical">
+          <ion-input
+            class="ion-margin-bottom"
+            fill="outline"
+            placeholder="mail@mail.com"/>
+          <ion-input
+            class="ion-margin-bottom"
+            fill="outline"
+            placeholder="********"/>
+          <button type="button" class="login-button">登入</button>
+          <div class="ion-text-center login-hint">Don't have an account? <span>Sign Up</span></div>
+        </div>
+        <div class="divider"></div>
+        <div class="ion-text-center login-other-way-hint">Or sign in with:</div>
+        <div class="ion-text-center">
+          <button type="button" class="login-google-button" @click="openGoogleLogin">
+            <ion-icon aria-hidden="true" color="danger" :icon="logoGoogle"/>
+          </button>
+        </div>
       </div>
     </ion-content>
   </ion-page>
@@ -18,7 +33,9 @@
 import {
   IonPage,
   IonContent,
-  IonButton
+  IonTitle,
+  IonInput,
+  IonIcon
 } from '@ionic/vue';
 import { ref } from 'vue';
 import { auth, googleProvider } from '../js/firebaseDB';
@@ -27,6 +44,7 @@ import { UserInfo } from '@/js/interface'
 import { useRouter } from 'vue-router';
 import { openToast } from '@/composible/util';
 import { useUserStore } from '@/store';
+import { logoGoogle } from 'ionicons/icons';
 
 const router = useRouter()
 const store = useUserStore()
@@ -67,6 +85,49 @@ const openGoogleLogin = async() => {
 
 <style lang="scss" scoped>
 .center-container {
-  height: 100%;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.login-title {
+  font-size: 42px;
+  font-weight: bold;
+  margin-bottom: 64px;
+}
+.login-button {
+  margin-top: 48px;
+  width: 100%;
+  height: 48px;
+  border-radius: 24px;
+  font-size: 16px;
+  background-color: #6366f1;
+}
+.login-hint {
+  margin-top: 8px;
+  color: #5f5f5f;
+  span {
+    color: #f59e0b;
+    font-weight: bold;
+  }
+}
+.divider {
+  margin-top: 64px;
+  margin-bottom: 24px;
+  width: 100%;
+  height: 1px;
+  background-color: #5f5f5f;
+}
+.login-other-way-hint {
+  margin-bottom: 16px;
+  color: #5f5f5f;
+}
+.login-google-button {
+  width: 160px;
+  height: 50px;
+  padding: 5px;
+  border-radius: 25px;
+  background-color: transparent;
+  border: 1px solid #ef4444;
 }
 </style>
