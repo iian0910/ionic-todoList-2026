7d4fa4a76faa6db762b608e72ec0677b09dbdf48
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2026-01-29 15:02:13 +0800

    Updates

--- assets/ios.transition-legacy-E6aqfpGV.js
+++ assets/ios.transition-legacy-E6aqfpGV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-kjq8bewt.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.ao,n=t.ap}],execute:function(){
+System.register(["./index-legacy-DoV5rB07.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.ao,n=t.ap}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-Cx7BzoRa.js
+++ assets/md.transition-legacy-Cx7BzoRa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-kjq8bewt.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.ap,i=e.ao}],execute:function(){
+System.register(["./index-legacy-DoV5rB07.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.ap,i=e.ao}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
